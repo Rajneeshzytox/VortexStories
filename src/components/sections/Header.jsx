cdea1aa6bfe2f4a31c3de2bfe55027da5dec45ad
Rajneesh Kumar <rajneesh.coder@gmail.com>
2024-07-19 14:45:15 +0530

    added info modal

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -1,22 +1,33 @@
-import { Search } from "lucide-react"
+import { Search } from "lucide-react";
+import InfoModal from "../ui/InfoModal";
 
-export default function Header(){
-    return(
-        <>
-        <header className="header min-h-20 h-fit flex justify-between items-center pl-20 py-4 gap-6 flex-wrap first-letter pr-10 min-w-52 sticky top-0 backdrop-blur-sm z-50">
-        {/* headee */}
-            <div className="capitalize ">
-                <h1 className="font-bold text-black">Vortex Stories</h1>
-                <p className="text-sm ">Share Your amazing stories</p>
-                
-            </div>
-           <div className="dropdown lg:dropdown-left dropdown-end">
-                <Search role="button" tabIndex={0}/>
-                <div className="mt-6 dropdown-content" tabIndex={0}>
-                    <input className="input input-primary input-bordered" type="text" placeholder="Search Stories" />
-                </div>
-           </div>
-        </header>
-        </>
-    )
+export default function Header() {
+  return (
+    <>
+      <header className="header min-h-20 h-fit flex justify-between items-center pl-20 py-4 gap-6 flex-wrap first-letter pr-10 min-w-52 sticky top-0 backdrop-blur-sm z-50">
+        {/* #### headee text start */}
+        <div className="capitalize ">
+          <h1 className="font-bold text-black">Vortex Stories</h1>
+          <p className="text-sm ">Share Your amazing stories</p>
+        </div>
+        {/* #### headee text ends */}
+
+        {/* #### Search DropDown start */}
+        <div className="dropdown lg:dropdown-left dropdown-end">
+          <Search role="button" tabIndex={0} />
+          <div className="mt-6 dropdown-content" tabIndex={0}>
+            <input
+              className="input input-primary input-bordered"
+              type="text"
+              placeholder="Search Stories"
+            />
+          </div>
+        </div>
+        {/* #### Search DropDown ends */}
+      </header>
+
+      {/* modal component */}
+      <InfoModal />
+    </>
+  );
 }
