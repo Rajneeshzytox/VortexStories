02d0a0ed6b33a66332ef52cbc3f5fb4e542cd8ba
Rajneesh Kumar <rajneesh.coder@gmail.com>
2024-07-20 16:28:58 +0530

    added top post data and render using props

--- src/components/sections/TopPost.jsx
+++ src/components/sections/TopPost.jsx
@@ -1,41 +1,73 @@
-
-
-
+import { topPostData } from "../../data/topPost";
+import { ArrowUpRightFromCircle, ArrowUpRight } from "lucide-react";
 
 export default function TopPost() {
   return (
     <>
       <aside className="mt-4">
         {/* section headings*/}
-        <div className="font-bold text-lg mb-4 ml-6">Featured Posts</div>
+        <div className="font-bold text-lg mb-4 ml-6 ">Top Posts</div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-2 bg-base-100 rounded-lg shadow-lg overflow-hidden pt-5 m-4   px-6 ">
-        {/* top post card */}
-        <div className="topPostCard flex gap-4 ">
-          {/* number  */}
-          <span className="text-base-300 text-2xl font-semibold">01</span>
-          {/* top post container */}
-          <div className="text-neutral ">
-            {/* topCard Title */}
-            <h5 className="font-bold text-primary lg:text-sm line-clamp-2">
-              Create Your First Ai Chatbot Using Gemini Api
-            </h5>
-            {/* topcard UserName */}
-            <span className="text-sm font-semibold lg:text-xs">{`Rajneesh Kumar`}</span>
-            <span className="text-sm mx-1 lg:text-xs">in</span>
-            {/* top card category */}
-            <span className="text-sm font-semibold lg:text-xs">{`Design `}</span>
-            {/* topcard details */}
-            <div className="w-full text-neutral text-xs">
-                <span>4 days ago</span>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-2 bg-base-100 rounded-lg shadow-lg overflow-hidden pt-5 m-4  px-6 ">
+
+          {/* Map (loop) of cards top post */}
+          {topPostData.map((post, index) => (
+            <div key={post.id} className="topPostCard flex gap-4 relative ">
+              {/* number  */}
+              <span className="text-base-300 text-xl font-semibold lg:text-md">{`0${
+                index + 1
+              }`}</span>
+
+              {/* top post container */}
+              <div className="text-neutral w-full PostCard">
+                {/* topCard Title */}
+                <h5 className="font-bold text-primary lg:text-sm line-clamp-2">
+                  {post.title}
+                </h5>
+
+                {/* topcard UserName */}
+                <span className="text-sm font-semibold lg:text-xs">
+                  {post.Author}
+                </span>
+
+                <span className="text-sm mx-1 lg:text-xs">in</span>
+
+                {/* top card category */}
+                <span className="text-sm font-semibold lg:text-xs">
+                  {post.Category}
+                </span>
+
+                {/* topcard details */}
+                <div className="w-full text-neutral text-xs">
+                  <span>{`${post.date} ago`}</span>
+                </div>
+
+                {/* link to visite Top post */}
+                <a
+                  href={post.href}
+                  className="VisitLink absolute top-1/2 -translate-y-6 right-16 opacity-0 transition-all lg:right-0 lg:-translate-y-4 btn btn-sm btn-circle hover:btn-primary origin-center"
+                >
+                  <ArrowUpRight />
+                </a>
+
+              {/* divider */}
+              <span className="divider divider-secondary w-full"></span>
             </div>
-            <span className="divider divider-secondary"></span>
-          </div>
-        </div>
         </div>
 
-
+          ))}
+        </div>
       </aside>
+
+
+    {/* Linkk IN animation top Post */}
+      <style jsx>{`
+        .PostCard:hover .VisitLink{
+            opacity: 1;
+            right: 2rem;
+        }
+        `}
+      </style>
     </>
   );
 }
---
+++ src/data/topPost.jsx
@@ -0,0 +1,34 @@
+export const topPostData = [
+    {
+        id: "topPost 1",
+        title: "Create Your First Ai Chatbot Using Gemini Api",
+        Author: "Rajneesh Kumar",
+        Category: "Design",
+        date: "4 days",
+        href: "#",
+    },
+    {
+        id: "topPost 2",
+        title: "Hello World in Machine Learning - a beginner guide",
+        Author: "John Doe",
+        Category: "Ai/Ml",
+        date: "2 min",
+        href: "#",
+    },
+    {
+        id: "topPost 3",
+        title: "What is lorem Ipsum?",
+        Author: "John Doe",
+        Category: "Web",
+        date: "8 min",
+        href: "#",
+    },
+    {
+        id: "topPost 4",
+        title: "Create Complex Layouts in just 3 lines of CSS",
+        Author: "Rajneesh Kumar",
+        Category: "Web",
+        date: "6 hour",
+        href: "#",
+    },
+]
