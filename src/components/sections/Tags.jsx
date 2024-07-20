76424192f7e03869d5d90e32d8e187ca79d5149b
Rajneesh Kumar <rajneesh.coder@gmail.com>
2024-07-20 19:31:07 +0530

    added tags components

--- src/components/sections/Tags.jsx
+++ src/components/sections/Tags.jsx
@@ -1,9 +1,23 @@
 
+const tags = ["Computer Science","React" ,"Science","app development", "javascript", "rust", "python", "ai", "web development", "Studies", "jee", "machine learning", "IIT", "Neet", "Technologies"]
 
 export default function Tags(){
     return(
         <>
-        <aside className=" ">tags</aside>
+        <aside className="px-4 py-8 static bottom-64">
+        <div className="font-bold text-lg mb-4 ml-1">Popular Tags</div>
+        
+        <div className="flex flex-wrap transition-all capitalize items-center px-2">
+
+            {
+                tags.map((tag) => (
+            <span key={`${tag} tag`} className="bg-base-200 rounded-md px-3 text-sm mx-3 my-[0.3rem] text-base-content hover:bg-primary hover:text-primary-content text-nowrap ">
+                {tag}
+            </span>
+                )) 
+            }
+        </div>
+        </aside>
         </>
     )
 }
