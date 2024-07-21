459ccc258720520acc6b0ffa761fcbed6d939945
Rajneesh Kumar <rajneesh.coder@gmail.com>
2024-07-21 13:57:23 +0530

    added darkMode colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,31 @@ export default {
   plugins: [require("daisyui")],
   daisyui: {
     themes: [
-      "dark",
+      {
+        darkMode: {    
+            "primary": "#f3f4f6",
+            "primary-content": "#141415",
+            "secondary": "#9ca3af",
+            "secondary-content": "#090a0b",
+            "accent": "#22d3ee", 
+            "accent-content": "#001014",   
+            "neutral": "#6b7280",
+            "neutral-content": "#e0e1e4",
+            "base-100": "#111827",  
+            "base-200": "#0d1320",  
+            "base-300": "#090f1a",
+            "base-content": "#c9cbcf",   
+            "info": "#3b82f6",   
+            "info-content": "#010615",  
+            "success": "#22c55e",    
+            "success-content": "#000e03",     
+            "warning": "#eab308", 
+            "warning-content": "#130c00",     
+            "error": "#f43f5e",
+  
+            "error-content": "#140103",
+        }
+      },
       {
         rajneesh: {
           primary: "#1f2937",
@@ -19,7 +43,6 @@ export default {
           neutral: "#6b7280",
           "neutral-content": "#e0e1e4",
           "base-100": "#ffffff",
-          "base-150": "#eeeeee",
           "base-200": "#dedede",
           "base-300": "#bebebe",
           "base-content": "#161616",
