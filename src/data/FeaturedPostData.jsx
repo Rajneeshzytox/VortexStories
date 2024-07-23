48a0b85386daebb94caa788100b63af4408ac952
Rajneesh Kumar <rajneesh.coder@gmail.com>
2024-07-23 16:30:31 +0530

    featured card data to map

---
+++ src/data/FeaturedPostData.jsx
@@ -0,0 +1,56 @@
+import { datePassed } from "../modules/datePassed"
+
+// NOTE: date format MM/DD/YY
+// ex: 12/30/2015
+
+
+export const FeaturedPostData = [
+    {
+        id: '1',
+        imageUrl: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dGVjaHxlbnwwfHwwfHx8MA%3D%3D',
+        avatarUrl: 'https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dGVjaHxlbnwwfHwwfHx8MA%3D%3D',
+        author: 'CodeX',
+        title: 'best web accessories to upgrade from beginner to advance',
+        IsOnline: false,
+        date: datePassed('01/22/2020'),
+        category: 'Tech',
+        tags: ["tech", "computer", "accessories"],
+
+    },
+    {
+        id: '1',
+        imageUrl: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dGVjaHxlbnwwfHwwfHx8MA%3D%3D',
+        avatarUrl: 'https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dGVjaHxlbnwwfHwwfHx8MA%3D%3D',
+        author: 'CodeX',
+        title: 'best web accessories to upgrade from beginner to advance',
+        IsOnline: false,
+        date: datePassed('01/22/2020'),
+        category: 'Tech',
+        tags: ["tech", "computer", "accessories"],
+
+    },
+    {
+        id: '1',
+        imageUrl: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dGVjaHxlbnwwfHwwfHx8MA%3D%3D',
+        avatarUrl: 'https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dGVjaHxlbnwwfHwwfHx8MA%3D%3D',
+        author: 'CodeX',
+        title: 'best web accessories to upgrade from beginner to advance',
+        IsOnline: false,
+        date: datePassed('01/22/2020'),
+        category: 'Tech',
+        tags: ["tech", "computer", "accessories"],
+
+    },
+    {
+        id: '1',
+        imageUrl: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dGVjaHxlbnwwfHwwfHx8MA%3D%3D',
+        avatarUrl: 'https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dGVjaHxlbnwwfHwwfHx8MA%3D%3D',
+        author: 'CodeX',
+        title: 'best web accessories to upgrade from beginner to advance',
+        IsOnline: false,
+        date: datePassed('01/22/2020'),
+        category: 'Tech',
+        tags: ["tech", "computer", "accessories"],
+
+    },
+]
