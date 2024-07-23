ad3766a1b712da3746298369196925fadf332017
Rajneesh Kumar <rajneesh.coder@gmail.com>
2024-07-23 16:27:35 +0530

    card modified for data map

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -1,32 +1,43 @@
-export default function PrimaryCard() {
+export default function PrimaryCard({data}) {
   return (
     <>
-      <div className="min-w-64 card bg-base-100 rounded-lg shadow-lg p-2">
-        <figure className="w-full h-32 bg-base-300 rounded-md">
+      <div className="min-w-64  card bg-base-100 rounded-lg mx-4 shadow-lg p-2">
+        <figure className="w-full ring h-32 overflow-hidden rounded-md image-full">
           {/* img here */}
+          <img src={data.imageUrl} alt={data.id} className="size-full w-44" />
         </figure>
 
         {/* Card text content */}
         <div className="mt-2 p-2 flex flex-col gap-3">
             {/* card info */}
             <div className="InfoCard text-xs text-neutral flex justify-between">
-                <span className="CardCategory">Success Stories</span>
-                <span className="DateOfPublish">4 days ago</span>
+              {/* Category here */}
+                <span className="CardCategory">{data.category}</span>
+                <span className="DateOfPublish">{data.date} ago</span>
             </div>
             {/* crard title */}
             <div className="TitleCard text-md font-semibold text-primary flex justify-between items-center pr-1">
-                <h4 className="CardTitle w-[80%] line-clamp-2 text-wrap leading-tight">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat.
+                <h4 className="CardTitle w-[80%] line-clamp-2 capitalize text-wrap leading-tight max-w-64">
+                {data.title}
                 </h4>
 
                 {/* avatar */}
-                <div className="avatar online">
-                <div className="w-8 rounded-full bg-secondary"></div>
+                <div className={` overflow-hidden w-8 rounded-full avatar ${(data.IsOnline)? 'online' : 'offline'}`}>
+                <img className="aspect-square w-4" src={data.avatarUrl} />
                 </div>
             </div>
             {/* Card Tags */}
             <div className="CardTags *:inline-block">
-                <a href="#" className="badge badge-secondary rounded-md px-4 ">Tag 1</a>
+                  {
+                    data.tags.map((tag) =>(
+                      <a href="#" className="badge badge-secondary rounded-md px-4 mx-1 ">
+
+                        {tag}
+                      </a>    
+                       
+                    ))
+                  }
+
             </div>
         </div>
       </div>
